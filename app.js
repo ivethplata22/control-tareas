4500b1f5a13dca9c1ac6c5b6b9a88f34349cb067
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-01 12:56:37 -0600

    Readline

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('colors');
-console.clear();
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async() => {
     console.log('Hola Mundo');
+    mostrarMenu();
+    pausa();
 }
 
 main();
