fe03d470be9844aba584dcecc4b84b28f917d8ba
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-01 13:05:44 -0600

    Repetir Menu Forma Infinita

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ require('colors');
 const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
 const main = async() => {
-    console.log('Hola Mundo');
-    mostrarMenu();
-    pausa();
+    let opt = '';
+
+    do {
+        opt = await mostrarMenu();
+        await pausa();
+    } while (opt !== '0');
 }
 
 main();
