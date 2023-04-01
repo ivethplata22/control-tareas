94c69f4337b017719102617b70100e03481a20d9
Iveth Plata <karen.platahdz2223@gmail.com>
2023-04-01 14:02:55 -0600

    Crear Tareas y Listar

--- app.js
+++ app.js
@@ -1,13 +1,41 @@
 require('colors');
-const { inquirerMenu, pausa } = require('./helpers/inquirer');
-const { Tareas } = require('./models/tareas');
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 const main = async() => {
     let opt = '';
+    const tareas = new Tareas();
 
     do {
         opt = await inquirerMenu();
-        console.log(opt);
+        
+        switch (opt) {
+            case '1':
+                // Crear Tarea
+                const desc = await leerInput('Descripción de la tarea: ');
+                tareas.crearTarea(desc);
+            break;
+            case '2':
+                // Listar Tareas
+                console.log(tareas._listado);
+            break;
+            case '3':
+                // Listar Tareas Completadas
+            break;
+            case '4':
+                // Listar Tareas Pendientes
+            break;
+            case '5':
+                // Completas Tarea(s)
+            break;
+            case '6':
+                // Borrar Tarea
+            break;
+            default:
+
+            break;
+        }
+
         await pausa();
     } while (opt !== '0');
 }
